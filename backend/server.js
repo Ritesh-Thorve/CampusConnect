b34b1d0501742c5292ca620019e8fd144461ddb0
Ritesh Thorve <riteshthorve2004@gmail.com>
2025-08-12 11:34:08 +0530

    fix: small double import issue

--- backend/server.js
+++ backend/server.js
@@ -18,9 +18,6 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Security & Logging
-import cors from "cors";
-
 app.use(cors({
   origin: "http://localhost:8080", 
   credentials: true
