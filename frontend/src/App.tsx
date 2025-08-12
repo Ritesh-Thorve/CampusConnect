deec98fd18dd0aeb31bdfc31b03ed7ce02d02f53
Ritesh Thorve <riteshthorve2004@gmail.com>
2025-08-12 13:43:11 +0530

    refactore: added toast and redux store

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,5 @@
-
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import AppToaster from "../src/components/AppToaster";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
@@ -18,8 +16,7 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
-      <Sonner />
+      <AppToaster />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './redux/store/store';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
@@ -14,6 +16,8 @@ if (!rootElement) {
 const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
