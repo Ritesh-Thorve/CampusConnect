851a0210efc76bf121f553afefa7cdf3bf96dd38
Ritesh Thorve <riteshthorve2004@gmail.com>
2025-08-17 18:01:23 +0530

    changes

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,5 @@
-import app from '../server';
+import app from '../server.js';
+import serverless from 'serverless-http';
 
-export default app;
+// Wrap Express app for Vercel serverless
+export default serverless(app);
