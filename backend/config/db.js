d0bd6e15a4d13c0cab087118de52d1af16428587
Ritesh Thorve <riteshthorve2004@gmail.com>
2025-08-17 22:45:48 +0530

    for serveless config

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,19 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+import { PrismaClient } from "@prisma/client";
+
+let prisma;
+
+if (process.env.NODE_ENV === "production") {
+  // Prevents creating many instances in Vercel’s serverless env
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+} else {
+  // Local dev
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
 export default prisma;
