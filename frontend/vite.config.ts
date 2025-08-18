f524692f649f0995e09360867ec58c602a31599d
Ritesh Thorve <riteshthorve2004@gmail.com>
2025-08-18 15:54:31 +0530

    added google get route

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,20 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: "::",
-    port: 8080,
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true
-      }
-    }
-  },
   build: {
-    outDir: "dist" 
+    outDir: "dist", // Vercel uses this folder
   },
   plugins: [react()],
   resolve: {
