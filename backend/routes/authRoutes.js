d57e7e959046749ad82fbb7dfc045b247eca43b7
Ritesh Thorve <riteshthorve2004@gmail.com>
2025-08-18 14:58:27 +0530

    added google get route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.post('/signup', authLimiter, signUp);
 router.post('/login', authLimiter,login);
-router.post('/google', googleAuth)
+router.post('/google', googleAuth);
+router.post('/google', googleAuth);
 
 export default router;
